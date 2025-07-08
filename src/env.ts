e50d486cf6fd42f665690cc3cbd459118c68c966
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2025-07-08 13:21:44 -0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(3333), //converte string para number
   DATABASE_URL: z.string().url().startsWith('postgresql://'),
   GEMINI_API_KEY: z.string(),
 })
