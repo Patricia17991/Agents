91c057db563e568d0a2eda2ca5968d2855809f5a
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2025-07-08 11:16:34 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,17 @@ import {
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
-} from "fastify-type-provide-zod"
+} from "fastify-type-provider-zod"
 
 import { fastifyCors } from "@fastify/cors"
 
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>() //criando nossa aplicação
+
+
+app.register(fastifyCors, {
+   origin: 'http://localhost:5173', //criando nossas rotas
+})
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
