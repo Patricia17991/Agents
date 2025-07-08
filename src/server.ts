ec27e7782de21c861837f68234b6a824820031e0
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2025-07-08 13:26:39 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
-import { fastify } from "fastify"
+import { fastify } from 'fastify'
 import {
-  type serializerCompiler,
-  type validatorCompiler,
+  serializerCompiler,
+  validatorCompiler,
   type ZodTypeProvider,
-} from "fastify-type-provider-zod"
+} from 'fastify-type-provider-zod'
 
-import { fastifyCors } from "@fastify/cors"
+import { fastifyCors } from '@fastify/cors'
+import { env } from './env.ts'
 
 
-const app = fastify().withTypeProvider<ZodTypeProvider>() //criando nossa aplicação
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 
 app.register(fastifyCors, {
-   origin: 'http://localhost:5173', //criando nossas rotas
+   origin: 'http://localhost:5173',
 })
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
-app.listen({ port:3333}).then(() => {
-   console.log('HTTP server Running!') //then p quando terminar de rodar notificar
-}) //aqui passamos a porta que nossa aplicação vai rodar
+app.get('/health', () => {
+   return 'OK' //todo servidor back-end acaba tendo essa rota
+})
+
+app.listen({ port: env.PORT}).then(() => {
+   console.log('HTTP server Running!') 
+}) 
