2f3fa7fda26263b4fca6f8180bedc587e81a57d7
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2025-07-08 14:25:56 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,37 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-
-import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
-
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
-
 app.register(fastifyCors, {
-   origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.get('/health', () => {
-   return 'OK' //todo servidor back-end acaba tendo essa rota para ver se o servidor está rodando
+  return 'OK'
 })
 
-app.listen({ port: env.PORT}).then(() => {
-   console.log('HTTP server Running!') 
-}) 
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
+
+app.listen({ port: env.PORT })
